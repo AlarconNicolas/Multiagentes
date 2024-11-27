0074559f89c3171290ed687366bc753820138866
Nicolas Alarcon Panopoulou <alarcon.panopoulou@gmail.com>
2024-11-27 12:56:08 -0600

    Update server.py

--- Server/trafficBase/server.py
+++ Server/trafficBase/server.py
@@ -16,7 +16,7 @@ def agent_portrayal(agent):
     if isinstance(agent, Road):
         portrayal["Color"] = "grey"
         portrayal["Layer"] = 0
-    
+
     if isinstance(agent, Destination):
         portrayal["Color"] = "lightgreen"
         portrayal["Layer"] = 0
@@ -45,26 +45,20 @@ def agent_portrayal(agent):
 width = 0
 height = 0
 
-with open('./static/city_files/2022_base.txt') as baseFile:
+with open('../static/city_files/2024_base.txt') as baseFile:
     lines = baseFile.readlines()
     width = len(lines[0]) - 1
     height = len(lines)
-
-# Define model parameters with N as an IntSlider to allow dynamic input
-model_params = {
-    "N": Slider("Number of Cars", 5, 1, 1000, 1)  # Creates a slider to select number of cars
-}
 
 # Create a CanvasGrid for visualization
 grid = CanvasGrid(agent_portrayal, width, height, 500, 500)
 
 agent_count_chart = ChartModule(
-    [{"Label": "Cars in Grid", "Color": "blue"},
-     {"Label": "Cars Reached Destination", "Color": "green"}],
+    [{"Label": "Cars in Grid", "Color": "blue"}],
     data_collector_name="datacollector"
 )
 
 # Set up the server with the bar chart and grid
-server = ModularServer(CityModel, [grid, agent_count_chart], "Traffic Simulation", model_params)
+server = ModularServer(CityModel, [grid, agent_count_chart], "Traffic Simulation", {"N":0})
 server.port = 8522  # The default port for Mesa
 server.launch()
